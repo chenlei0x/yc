e2d8f425dab42d8ec3a5c5f38b1e65c79d9cfe05
lennychen <lennychen@tencent.com>
2021-03-30 12:21:11 +0800

    install dependent whl

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pipi install ./lxml-4.6.3-cp35-cp35m-manylinux1_x86_64.whl
+pip3 install ./lxml-4.6.3-cp35-cp35m-manylinux1_x86_64.whl
 pip3 install ./et_xmlfile-1.0.1-cp35-none-any.whl
 pip3 install ./openpyxl-3.0.7.1-py2.py3-none-any.whl
