435f8f752cf518876f8044c08191d0c1e6279ad7
lennychen <lennychen@tencent.com>
2021-03-30 12:21:40 +0800

    add deinit.sh

---
+++ deinit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip3 uninstall -y ./lxml-4.6.3-cp35-cp35m-manylinux1_x86_64.whl
+pip3 uninstall -y ./et_xmlfile-1.0.1-cp35-none-any.whl
+pip3 uninstall -y ./openpyxl-3.0.7.1-py2.py3-none-any.whl
