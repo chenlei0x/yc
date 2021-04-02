51fc41ff621548f63aa25eddcb818e15b6aaa3f2
lennychen <lennychen@tencent.com>
2021-04-02 11:53:07 +0800

    remove deinit init.sh

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip3 install whl/lxml-4.6.3-cp35-cp35m-manylinux1_x86_64.whl
-pip3 install whl/et_xmlfile-1.0.1-cp35-none-any.whl
-pip3 install whl/openpyxl-3.0.7.1-py2.py3-none-any.whl
